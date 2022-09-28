353c0ea3fcfc1fe5261317e32ae7ad91e6a8df37
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-09-28 03:53:45 +0100

    update project design

--- js/speakers.js
+++ js/speakers.js
@@ -2,30 +2,44 @@ const speakers = document.getElementById('speakers');
 const speakerData = [
   {
     name: 'Habeeb Efiamotu Musa',
-    role: 'Fullstack web developer',
-    description: 'Microverse',
+    role: 'Student, University of Lagos, Nigeria',
+    description: '400 Level Law student with keen interest in solving global issues around the world.',
     image: './images/profile-img.jpg',
   },
 
   {
-    name: 'Mark Zuckerberg',
-    role: 'Enterpreneur',
-    description: 'Co-founder of facebook and its parent company Meta Platforms',
-    image: './images/mark-zuck.jpeg',
+    name: 'Mark Steinsein',
+    role: 'Student, University of Benin republic, Cotonou',
+    description: 'Final year Accounting student with interest in solving global issues around the world',
+    image: './images/profile.jpg',
   },
 
   {
     name: 'Billy Gilmour',
-    role: 'Midfielder',
-    description: 'Footballer for Chelsea Football Club, London, England',
-    image: './images/gilmour.jpeg',
+    role: 'Student, University of Ghana, Legwon',
+    description: 'Second year Mass Communication student with keen interest in solving global issues around the world',
+    image: './images/speakers/ryan.jpg',
   },
 
   {
     name: 'Ernie Williams',
-    role: 'Enterpreneur',
-    description: 'Founder. New Horizon',
-    image: './images/profile.jpg',
+    role: 'Student, University of Pretoria, South Africa',
+    description: 'First year Law student with keen interest in solving global issues around the world',
+    image: './images/speakers/jordan.jpg',
+  },
+
+  {
+    name: 'rauch',
+    role: 'Student, University of California, Usa',
+    description: 'Fourth year Law student with keen interest in solving global issues around the world',
+    image: './images/speakers/rauch.png',
+  },
+
+  {
+    name: 'Isaac',
+    role: 'Student, University of columbia, Usa',
+    description: 'First year medical student with keen interest in solving global issues around the world',
+    image: './images/speakers/isaac.jpg',
   },
 
 ];
@@ -47,3 +61,23 @@ speakers.innerHTML = speakerData.map((speak) => (
 </li>
     `
 )).join('');
+
+if (speakerData.length >= 2) {
+  const speakersList = document.querySelectorAll('.single-speaker');
+  const speakersListArray = Array.from(speakersList);
+  const slicedSpeakers = speakersListArray.splice(2);
+  slicedSpeakers.forEach((speaker) => {
+    speaker.classList.add('d-none-speaker');
+  });
+}
+
+const show = document.querySelector('.show-more');
+show.addEventListener('click', () => {
+  const speakersList = document.querySelectorAll('.single-speaker');
+  const speakersListArray = Array.from(speakersList);
+  const slicedSpeakers = speakersListArray.splice(2);
+  slicedSpeakers.forEach((speaker) => {
+    speaker.classList.remove('d-none-speaker');
+    show.style.display = 'none';
+  });
+});
